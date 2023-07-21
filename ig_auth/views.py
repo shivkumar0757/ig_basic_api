6c326ce56fefaf0efb7b05bb8c0aa492d0ad76b1
shiv <shivkumar0757@gmail.com>
2023-07-21 13:16:52 +0530

    initial commit

---
+++ ig_auth/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def home(request):
+    context = {
+        'client_id': '684477648739411',  # Replace with your client_id
+        'redirect_uri': 'https://socialsizzle.herokuapp.com/auth/',  # Replace with your redirect_uri
+    }
+    return render(request, 'home.html', context)
+
+def auth(request):
+    full_url = request.build_absolute_uri()
+    base_url = request.build_absolute_uri('/')[:-1].strip("/")
+    code = request.GET.get('code', '')
+
+    data = {
+        'full_url': full_url,
+        'base_url': base_url,
+        'parameters': {
+            'code': code,
+        },
+    }
+    return JsonResponse(data, json_dumps_params={'indent': 2})
