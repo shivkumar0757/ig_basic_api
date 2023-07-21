135ff7af11667f0bf2484f59086ac0c83e41c6fb
shiv <shivkumar0757@gmail.com>
2023-07-21 14:09:48 +0530

    adding env variables

--- ig_auth/views.py
+++ ig_auth/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from urllib.parse import urlparse, parse_qs
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
 
 def home(request):
     context = {
-        'client_id': '684477648739411',  # Replace with your client_id
-        'redirect_uri': 'https://socialsizzle.herokuapp.com/auth/',  # Replace with your redirect_uri
+        'client_id': CLIENT_ID,  # Replace with your client_id
+        'redirect_uri': REDIRECT_URI,  # Replace with your redirect_uri
     }
     return render(request, 'home.html', context)
 
 
-from urllib.parse import urlparse, parse_qs
 
 def auth(request):
     full_url = request.build_absolute_uri()
@@ -25,17 +32,3 @@ def auth(request):
     }
     return render(request, 'auth.html', context)
 
-
-# def auth(request):
-#     full_url = request.build_absolute_uri()
-#     base_url = request.build_absolute_uri('/')[:-1].strip("/")
-#     code = request.GET.get('code', '')
-
-#     data = {
-#         'full_url': full_url,
-#         'base_url': base_url,
-#         'parameters': {
-#             'code': code,
-#         },
-#     }
-#     return JsonResponse(data, json_dumps_params={'indent': 2})
